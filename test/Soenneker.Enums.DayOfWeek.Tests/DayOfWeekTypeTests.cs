10cbf3dad0065a8b7e190ec8b960b864350da9bc
Jake Soenneker <jake@soeneker.com>
2024-12-22 14:01:49 +0000

    Tests

---
+++ test/Soenneker.Enums.DayOfWeek.Tests/DayOfWeekTypeTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Enums.DayOfWeek.Tests;
+
+[Collection("Collection")]
+public class DayOfWeekTypeTests : FixturedUnitTest
+{
+    public DayOfWeekTypeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
