6f76ea07d00b959a2f6afd8d96e93d98fba70f7c
Jake Soenneker <jake@soenneker.com>
2025-02-22 13:17:13 -0600

    SmartEnum -> Intellenum

--- src/DayOfWeekType.cs
+++ src/DayOfWeekType.cs
@@ -1,21 +1,18 @@
-﻿using Soenneker.SmartEnum.Named;
+﻿using Intellenum;
 
 namespace Soenneker.Enums.DayOfWeek;
 
 /// <summary>
 /// An enumerator for describing the day of the week (Mon, Tues, ..)
 /// </summary>
-public sealed class DayOfWeekType : NamedSmartEnum<DayOfWeekType>
+[Intellenum<string>]
+public partial class DayOfWeekType
 {
-    public static readonly DayOfWeekType Sunday = new(nameof(Sunday), 0);
-    public static readonly DayOfWeekType Monday = new(nameof(Monday), 1);
-    public static readonly DayOfWeekType Tuesday = new(nameof(Tuesday), 2);
-    public static readonly DayOfWeekType Wednesday = new(nameof(Wednesday), 3);
-    public static readonly DayOfWeekType Thursday = new(nameof(Thursday), 4);
-    public static readonly DayOfWeekType Friday = new(nameof(Friday), 5);
-    public static readonly DayOfWeekType Saturday = new(nameof(Saturday), 6);
-
-    private DayOfWeekType(string name, int value) : base(name, value)
-    {
-    }
+    public static readonly DayOfWeekType Sunday = new(nameof(Sunday));
+    public static readonly DayOfWeekType Monday = new(nameof(Monday));
+    public static readonly DayOfWeekType Tuesday = new(nameof(Tuesday));
+    public static readonly DayOfWeekType Wednesday = new(nameof(Wednesday));
+    public static readonly DayOfWeekType Thursday = new(nameof(Thursday));
+    public static readonly DayOfWeekType Friday = new(nameof(Friday));
+    public static readonly DayOfWeekType Saturday = new(nameof(Saturday));
 }
