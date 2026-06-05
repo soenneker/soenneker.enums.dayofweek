8fd489edfc5a204714e85d38e697e8cfad71e0fd
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:29 -0500

    xml commenting

--- src/Soenneker.Enums.DayOfWeek/DayOfWeekType.cs
+++ src/Soenneker.Enums.DayOfWeek/DayOfWeekType.cs
@@ -10,11 +10,32 @@ namespace Soenneker.Enums.DayOfWeek;
 [EnumValue<string>]
 public sealed partial class DayOfWeekType
 {
+    /// <summary>
+    /// The sunday.
+    /// </summary>
     public static readonly DayOfWeekType Sunday = new(nameof(Sunday));
+    /// <summary>
+    /// The monday.
+    /// </summary>
     public static readonly DayOfWeekType Monday = new(nameof(Monday));
+    /// <summary>
+    /// The tuesday.
+    /// </summary>
     public static readonly DayOfWeekType Tuesday = new(nameof(Tuesday));
+    /// <summary>
+    /// The wednesday.
+    /// </summary>
     public static readonly DayOfWeekType Wednesday = new(nameof(Wednesday));
+    /// <summary>
+    /// The thursday.
+    /// </summary>
     public static readonly DayOfWeekType Thursday = new(nameof(Thursday));
+    /// <summary>
+    /// The friday.
+    /// </summary>
     public static readonly DayOfWeekType Friday = new(nameof(Friday));
+    /// <summary>
+    /// The saturday.
+    /// </summary>
     public static readonly DayOfWeekType Saturday = new(nameof(Saturday));
 }
